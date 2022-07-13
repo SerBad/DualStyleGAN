847438cae0d49318f255021e9238558410de7002
zhou <zhoutjcc@msn.cn>
2022-07-13 22:28:44 +0800

    提交保存网络的代码

--- trans_pt.py
+++ trans_pt.py
@@ -18,7 +18,7 @@ import io
 
 
 def save_jit():
-    device = "cpu"
+    device = "cuda"
     root = "/kaggle/input/zhoudualstylegan/DualStyleGAN/"
     pt_path = root + "checkpoint/head2-copy/generator-001500.pt"
     # generator = DualStyleGAN()
