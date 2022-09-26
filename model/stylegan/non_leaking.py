71dd62bb10265ea09255565f6f767cb7eaea8621
zhou <zhoutjcc@msn.cn>
2022-09-26 20:17:03 +0800

    cuda

--- model/stylegan/non_leaking.py
+++ model/stylegan/non_leaking.py
@@ -358,8 +358,9 @@ class GridSampleBackward(autograd.Function):
     @staticmethod
     def forward(ctx, grad_output, input, grid):
         op = torch._C._jit_get_operation("aten::grid_sampler_2d_backward")
-        print("GridSampleBackward", op)
+        print("GridSampleBackward1==", op)
         grad_input, grad_grid = op(grad_output, input, grid, 0, 0, False)
+        print("GridSampleBackward2==", grad_input, grad_grid)
         ctx.save_for_backward(grid)
 
         return grad_input, grad_grid
